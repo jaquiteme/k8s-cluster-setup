d7a4d84486b1f85f59b3fb5a24d3f8b1f6802f7b
Jordy <jordyaquiteme@gmail.com>
2023-03-13 10:35:08 +0100

    Add k8s scripts

---
+++ provisioning/scripts/init-cluster.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#=====================
+#| INIT K8s CLUSTER  |
+#=====================
+# /!\ DONT RUN THIS SCRIPT AS SUDO USER
+
+K8S_RELEASED_VERSION="{$1:-1.26.0}"
+POD_NET_CIDR="{$2:-192.168.0.0/16}"
+API_SERVER_ADDR="{$3:-$(/bin/hostname -i)}"
+
+sudo kubectl get nodes
+# Check if the cluster is already init
+if [[ $? != 0 ]]; then
+  # Setup the cluster
+  sudo kubeadm init \
+    --apiserver-advertise-address "$API_SERVER_ADDR" \
+    --pod-network-cidr "$POD_NET_CIDR" \
+    --kubernetes-version "$K8S_RELEASED_VERSION"
+fi
+
+if [[ $? -eq 0 ]]; then
+  USER=$(id -u)
+  GROUP=$(id -g)
+  echo "Start configuring kubectl for user: $(whoami)"
+  # Allow current user to execute K8s commands
+  USER_K8S_HOME=$HOME/.kube
+  K8S_HOME=/etc/kubernetes
+  mkdir -p $USER_K8S_HOME
+  sudo mkdir -p $K8S_HOME
+  sudo cp -i $K8S_HOME/admin.conf $USER_K8S_HOME/config
+  sudo chown $USER:$GROUP $USER_K8S_HOME/config
+fi
+
+# Installing Networking Plugin Calico
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+
+# Print the control node join command
+echo "[WORKERS JOIN COMMAND] => copy and paste the command below on your worker nodes"
+kubeadm token create --print-join-command
---
+++ provisioning/scripts/setup-k8s.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#======================================
+#| K8S SETUP FOR DEBIAN BASED DISTRIB | 
+#======================================
+
+K8S_VERSION="{$1:-1.26.0-00}"
+
+echo "Setting up containerd required modules..."
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay && modprobe br_netfilter
+
+echo "Setting up systcl conf..."
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+update_sysctl=$(sysctl --system)
+if [[ $? -eq 0 ]]; then
+   echo "Sysctl successfully reloaded"
+fi
+
+# Install containerd.io following docker installation
+# https://forum.linuxfoundation.org/discussion/862825/kubeadm-init-error-cri-v1-runtime-api-is-not-implemented
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update && sudo apt-get install -y containerd.io
+# Removing default containerd config if exists
+if [[ -f "/etc/containerd/config.toml" ]]; then
+  rm /etc/containerd/config.toml
+fi
+# Starting containerd service
+systemctl start containerd
+
+# K8S need swap to be disabled
+swapoff -a
+
+# Miscellaneous packages required by K8S
+apt-get update && sudo apt-get install -y ca-certificates apt-transport-https curl
+
+# Download Google cloud public signing key
+mkdir -p /etc/apt/keyrings
+curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+# Add K8S apt 
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Install K8S packages
+apt-get update && apt-get install -y kubelet="$K8S_VERSION" kubeadm="$K8S_VERSION" kubectl="$K8S_VERSION"
+
+# Freeze K8s package versions
+apt-mark hold kubelet kubeadm kubectl
